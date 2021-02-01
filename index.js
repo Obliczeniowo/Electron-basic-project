eab04d70a4f3b5be27f704f06b8f8e3358b75d61
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2021-02-01 05:21:37 +0100

    add win variable

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const { Messages } = require("./electronscripts/messages");
 // ustawia produkcyjną wersję
 process.env.NODE_ENV = "dev"; // 'production';
 
+let win
+
 /**
  * Funkcja tworząca okno programu
  */
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
